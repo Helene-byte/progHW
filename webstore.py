f47d8b43da3dfc6f9920e2fedc64eae8d42fb754
Olena Krasylshchykova <olena_krasylshchykova@epam.com>
2022-01-30 20:02:25 +0200

    PEP8 fixes

--- webstore.py
+++ webstore.py
@@ -1,10 +1,11 @@
 class Item:
-    '''
+    """
     Создайте пользовательский класс для описания товара (предположим,
 это задел для интернет-магазина). В качестве полей товара можете
 использовать значение цены, описание, габариты товара. Создайте пару
 экземпляров вашего класса и протестируйте их работу.
-    '''
+    """
+
     def __init__(self, item, price, color, size):
         self.item = item
         self.price = price
@@ -12,12 +13,12 @@ class Item:
         self.size = size
 
     def __str__(self):
-        return "\n"\
-            "Item name: " + self.item + "\n" \
+        return "\n" \
+               "Item name: " + self.item + "\n" \
                + "Price: " + str(self.price) + "\n" \
                + "Color: " + self.color + "\n" \
-               + "Size: " + self.size+"\n" \
-                + "+++++++++++++++++++++++"
+               + "Size: " + self.size + "\n" \
+               + "+++++++++++++++++++++++"
 
 
 class Customer:
@@ -25,6 +26,7 @@ class Customer:
     Создайте класс «Покупатель». В качестве полей можете использовать
     фамилию, имя, отчество, мобильный телефон и т. д.
     """
+
     def __init__(self, surname, name, phone, address):
         self.surname = surname
         self.name = name
@@ -38,6 +40,7 @@ class Customer:
                + "Address: " + self.address + "\n" \
                + "+++++++++++++++++++++++"
 
+
 class Order:
     """
     Заказ может содержать несколько товаров.
@@ -45,6 +48,7 @@ class Order:
 Реализуйте метод вычисления суммарной стоимости заказа. Определите
 метод __str__() для корректного вывода информации о этом заказе.
     """
+
     def __init__(self, customer, items, quantity):
         self.customer = customer
         self.items = items
@@ -54,23 +58,24 @@ class Order:
 
         return "Customer: " + str(vars(self.customer)) + "\n" \
                + "Items: " + ('[%s]' % ', '.join(map(str, self.items))) + "\n" \
-               + "Quantity : " + ( '[%s]' % ', '.join(map(str, self.quantity))) + "\n" \
-               + "Total amount : " + str(self.total_amount(items = self.items, quantity = self.quantity)) + "\n" \
+               + "Quantity : " + ('[%s]' % ', '.join(map(str, self.quantity))) + "\n" \
+               + "Total amount : " + str(self.total_amount(items=self.items, quantity=self.quantity)) + "\n" \
                + "+++++++++++++++++++++++"
 
-    def total_amount(self,items,quantity):
-        total=[]
-        items_price=[]
+    def total_amount(self, items, quantity):
+        total = []
+        items_price = []
         for item in items:
             items_price.append(item.price)
         for num1, num2 in zip(items_price, quantity):
             total.append(num1 * num2)
         return sum(total)
 
-item1=Item("D1",50.50, "red", "M")
-item2=Item("D1",45, "white", "M")
-customer1=Customer("Bloggs", "Joe", "380661112233", "NY, Brooklyn")
-customer2=Customer("Jones", "Bridgit", "380661112235", "London, BY")
-order1=Order(customer1,[item2,item2],[1,1])
+
+item1 = Item("D1", 50.50, "red", "M")
+item2 = Item("D1", 45, "white", "M")
+customer1 = Customer("Bloggs", "Joe", "380661112233", "NY, Brooklyn")
+customer2 = Customer("Jones", "Bridgit", "380661112235", "London, BY")
+order1 = Order(customer1, [item2, item2], [1, 1])
 
 print(order1)
